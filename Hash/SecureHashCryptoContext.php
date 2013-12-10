f3b5b564fcb369eee59324d07354c6571e1d0f23
Sean Quinn <swquinn@gmail.com>
2013-12-09 23:32:50 -0500

    Update license comments and fix the SecureHashCryptoContext.

--- Hash/SecureHashCryptoContext.php
+++ Hash/SecureHashCryptoContext.php
@@ -1,4 +1,30 @@
 <?php
+/* Copyright (c) 2013 Rhapsody Project
+ *
+ * Licensed under the MIT License (http://opensource.org/licenses/MIT)
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 namespace Rhapsody\CryptoBundle;
 
 use Rhapsody\CryptoBundle\Context\ICryptoContext;
@@ -7,13 +33,19 @@ use Rhapsody\CryptoBundle\Hash\SecureHashProvider;
 /**
  * @author    Sean W. Quinn <sean.quinn@extesla.com>
  * @category  Crypto
- * @package   Rhapsody\Component\Crypto
- * @copyright Copyright (c) 2012 Rhapsody Project
+ * @package   Rhapsody\CryptoBundle\Hash
+ * @copyright Copyright (c) 2013 Rhapsody Project
  * @version   $Id$
  * @since     1.0
  */
-class CryptoContext implements ICryptoContext
+class SecureHashCryptoContext implements ICryptoContext
 {
+
+	/**
+	 *
+	 * @var ICryptoProvider
+	 * @access protected
+	 */
 	protected $provider;
 
 	public function __construct($provider)
@@ -21,20 +53,24 @@ class CryptoContext implements ICryptoContext
 		if (!($provider instanceof SecureHashProvider)) {
 			throw new \Exception();
 		}
+		$this->provider = $provider;
 	}
 
-	public function check($data, $encrypted) {
+	public function check($data, $encrypted)
+	{
 		return $this->provider->check($data, $encrypted);
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function encrypt($data, $salt = null, $encoding = null) {
+	public function encrypt($data, $salt = null, $encoding = null)
+	{
 		return $this->provider->encrypt($data, $salt, $encoding);
 	}
 
-	public function decrypt($encrypted, $encoding = null) {
+	public function decrypt($encrypted, $encoding = null)
+	{
 		// TODO
 	}
 }
--- Hash/SecureHashProvider.php
+++ Hash/SecureHashProvider.php
@@ -1,4 +1,30 @@
 <?php
+/* Copyright (c) 2013 Rhapsody Project
+ *
+ * Licensed under the MIT License (http://opensource.org/licenses/MIT)
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 namespace Rhapsody\CryptoBundle\Hash;
 
 use \Rhapsody\CryptoBundle\Provider\ICryptoProvider;
@@ -8,8 +34,8 @@ use \Rhapsody\CryptoBundle\Hash\SecureHash;
  *
  * @author    Sean W. Quinn <sean.quinn@Rhapsody.org>
  * @category  Crypto
- * @package   Rhapsody\Component\Crypto\Tests\Hash
- * @copyright Copyright (c) 2012 Rhapsody Project
+ * @package   Rhapsody\CryptoBundle\Hash
+ * @copyright Copyright (c) 2013 Rhapsody Project
  * @version   $Id$
  * @since     1.0
  */
@@ -91,7 +117,8 @@ final class SecureHashProvider implements ICryptoProvider
 	 * 			see if there is a match.
 	 * @return True if this hash may have been derived from the clear data. False, otherwise.
 	 */
- 	public function /*bool*/ check($data, $secureHash) {
+ 	public function /*bool*/ check($data, $secureHash)
+	{
  		$candidateHashValue = $this->computeHash($data, $secureHash->getRandomSalt());
  		return $secureHash->getHashValue() === $candidateHashValue;
  	}
